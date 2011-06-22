689281b31ebed190ccd198214a180b2db83727aa
Peter Busse <pbusse@informatik.uni-bremen.de>
2011-06-22 16:29:13 +0200

    added top sonar that should work now. prepared stuff for bottom sonar. modified scene.

---
+++ tasks/SonarTop.cpp
@@ -0,0 +1,73 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "SonarTop.hpp"
+#include "Simulation.h"
+
+using namespace avalon_simulation;
+
+SonarTop::SonarTop(std::string const& name, TaskCore::TaskState initial_state)
+    : SonarTopBase(name, initial_state)
+{
+}
+
+SonarTop::SonarTop(std::string const& name, RTT::ExecutionEngine* engine, TaskCore::TaskState initial_state)
+    : SonarTopBase(name, engine, initial_state)
+{
+}
+
+SonarTop::~SonarTop()
+{
+}
+
+bool SonarTop::startHook()
+{
+    if (!SonarTopBase::startHook())
+        return false;
+
+    if(!Simulation::isInitialized())
+        throw std::runtime_error("Cannot start sonar data. The simulator is not running in the same process.");
+    
+    return true;
+}
+
+void SonarTop::updateHook()
+{
+  SonarTopBase::updateHook();
+  
+    if(Simulation::getAvalonPlugin()->getTopSonarData(&sonarscan)){
+      
+    }
+    else{
+      sonarscan.scanData.clear();
+    }
+}
+
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See SonarTop.hpp for more detailed
+// documentation about them.
+
+// bool SonarTop::configureHook()
+// {
+//     if (! SonarTopBase::configureHook())
+//         return false;
+//     return true;
+// }
+
+// void SonarTop::updateHook()
+// {
+//     SonarTopBase::updateHook();
+// }
+// void SonarTop::errorHook()
+// {
+//     SonarTopBase::errorHook();
+// }
+// void SonarTop::stopHook()
+// {
+//     SonarTopBase::stopHook();
+// }
+// void SonarTop::cleanupHook()
+// {
+//     SonarTopBase::cleanupHook();
+// }
+
