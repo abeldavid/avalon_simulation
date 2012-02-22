33f7c48389ef23ddd2c53c4d94b5731b5e4834ab
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2012-02-22 21:00:18 +0100

    new push for new sensor interface

--- scripts/run.rb
+++ scripts/run.rb
@@ -3,7 +3,7 @@ include Orocos
 
 Orocos.initialize
 
-#widget = Vizkit.load "simulator.ui"
+widget = Vizkit.load "simulator.ui"
 
 Orocos.run "AvalonSimulation" ,:wait => 10 do 
     simulation = TaskContext.get 'avalon_simulation'
@@ -30,36 +30,36 @@ Orocos.run "AvalonSimulation" ,:wait => 10 do
     state_estimator = TaskContext.get 'state_estimator_simulation'
     state_estimator.configure
     state_estimator.start
-#    Vizkit.display sonar.sonar_beam, :widget => widget.sonar_top
-#    Vizkit.display sonar_rear.sonar_beam, :widget => widget.sonar_rear
-#    Vizkit.display state_estimator.pose_samples, :widget => widget.orientation
+    Vizkit.display sonar.sonar_beam, :widget => widget.sonar_top
+    #Vizkit.display sonar_rear.sonar_beam, :widget => widget.sonar_rear
+    Vizkit.display state_estimator.pose_samples, :widget => widget.orientation
 
-#    widget.joystick1.connect(SIGNAL('axisChanged(double,double)'))do |x,y|
-#        sample = writer.new_sample
-#        sample.time = Time.now 
-#        0.upto(5) do
-#            sample.mode << :DM_PWM
-#            sample.target << 0;
-#        end
-#        sample.target[2] = -x
-#        sample.target[3] = -x
-#        sample.target[4] = y
-#        writer.write sample
-#    end
-#
-#    widget.joystick2.connect(SIGNAL('axisChanged(double,double)'))do |x,y|
-#        sample = writer.new_sample
-#        sample.time = Time.now 
-#        0.upto(5) do
-#            sample.mode << :DM_PWM
-#            sample.target << 0;
-#        end
-#        sample.target[1] = -x
-#        sample.target[5] = y
-#        writer.write sample
-#    end
-#
-#    widget.show 
+    widget.joystick1.connect(SIGNAL('axisChanged(double,double)'))do |x,y|
+        sample = writer.new_sample
+        sample.time = Time.now 
+        0.upto(5) do
+            sample.mode << :DM_PWM
+            sample.target << 0;
+        end
+        sample.target[2] = -x
+        sample.target[3] = -x
+        sample.target[4] = y
+        writer.write sample
+    end
+
+    widget.joystick2.connect(SIGNAL('axisChanged(double,double)'))do |x,y|
+        sample = writer.new_sample
+        sample.time = Time.now 
+        0.upto(5) do
+            sample.mode << :DM_PWM
+            sample.target << 0;
+        end
+        sample.target[1] = -x
+        sample.target[5] = y
+        writer.write sample
+    end
+    #Vizkit.display front_cam
+    widget.show 
     Vizkit.exec
 end
 
