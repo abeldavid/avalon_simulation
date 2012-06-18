a218343f0cbda704c67db1c11af908a892f7a27e
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2012-06-18 15:55:51 +0200

    Added rougth_asv control

--- scripts/run.rb
+++ scripts/run.rb
@@ -5,7 +5,8 @@ Orocos.initialize
 
 widget = Vizkit.load "simulator.ui"
 
-Orocos.run "AvalonSimulation" ,:wait => 10000, :valgrind => false, :valgrind_options => ['--undef-value-errors=no'] do 
+#Orocos.run "AvalonSimulation" ,:wait => 10000, :valgrind => false, :valgrind_options => ['--undef-value-errors=no'] do 
+#Orocos.run "AvalonSimulation" ,:wait => 10000, :valgrind => false, :valgrind_options => ['--undef-value-errors=no'] do 
     simulation = TaskContext.get 'avalon_simulation'
     front_cam = TaskContext.get 'front_camera'
     bottom_cam = TaskContext.get 'bottom_camera'
@@ -31,9 +32,13 @@ simulation.scenefile = "#{ENV['AUTOPROJ_PROJECT_BASE']}/simulation/orogen/avalon
 
 
     actuactors = TaskContext.get 'actuators'
+    asv_actuactors = TaskContext.get 'asv_actuators'
+    asv_actuactors.configure
     actuactors.configure
     actuactors.start
+    asv_actuactors.start
     writer = actuactors.command.writer
+    asv_writer = asv_actuactors.command.writer
 
     sonar = TaskContext.get 'sonar'
     sonar.configure
@@ -51,14 +56,15 @@ simulation.scenefile = "#{ENV['AUTOPROJ_PROJECT_BASE']}/simulation/orogen/avalon
     widget.joystick1.connect(SIGNAL('axisChanged(double,double)'))do |x,y|
         sample = writer.new_sample
         sample.time = Time.now 
-        0.upto(5) do
+        0.upto(3) do
             sample.mode << :DM_PWM
             sample.target << 0;
         end
-        sample.target[2] = -x
-        sample.target[3] = -x
-        sample.target[4] = y
-        writer.write sample
+        sample.target[0] = x
+        sample.target[1] = x
+        sample.target[2] = y
+        sample.target[3] = y
+        asv_writer.write sample
     end
 
     widget.joystick2.connect(SIGNAL('axisChanged(double,double)'))do |x,y|
@@ -87,9 +93,10 @@ simulation.scenefile = "#{ENV['AUTOPROJ_PROJECT_BASE']}/simulation/orogen/avalon
     Vizkit.display sonar_rear
     Vizkit.display pinger_search
     Vizkit.display actuactors
+    Vizkit.display asv_actuactors
     widget.show 
     Vizkit.exec
 
 
-end
+#end
 
