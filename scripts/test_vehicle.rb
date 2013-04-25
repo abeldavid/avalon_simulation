ac947accedaa45e830df6e03d113a6053cc45c95
Tilo Eichhorn <tilodsi@tzi.de>
2013-04-25 17:27:26 +0200

    Finished test vehicle development

--- scripts/test_vehicle.rb
+++ scripts/test_vehicle.rb
@@ -7,7 +7,7 @@ widget = Vizkit.load "simulator.ui"
 
 Orocos.run "AvalonSimulation" ,:wait => 10000, :valgrind => false, :valgrind_options => ['--undef-value-errors=no'] do 
     simulation = TaskContext.get 'avalon_simulation'
-    simulation.scenefile = "#{ENV['AUTOPROJ_PROJECT_BASE']}/simulation/orogen/avalon_simulation/configuration/test_vehicle.scn"
+    simulation.scenefile = "#{ENV['AUTOPROJ_PROJECT_BASE']}/simulation/orogen/avalon_simulation/configuration/avalon.scn"
 
     simulation.debug_sonar = false 
     simulation.use_osg_ocean = false 
@@ -18,23 +18,48 @@ Orocos.run "AvalonSimulation" ,:wait => 10000, :valgrind => false, :valgrind_opt
     test_vehicle_actuactors.configure
     test_vehicle_actuactors.start
     test_vehicle_writer = test_vehicle_actuactors.command.writer
+		
+		sample = test_vehicle_writer.new_sample
+		sample.time = Time.now 
+		0.upto(5) do
+			sample.mode << :DM_PWM
+			sample.target << 0;
+		end
 
-    widget.joystick1.connect(SIGNAL('axisChanged(double,double)'))do |x,y|
-        sample = test_vehicle_writer.new_sample
-        sample.time = Time.now 
-        0.upto(3) do
-            sample.mode << :DM_PWM
-            sample.target << 0;
-        end
-        sample.target[0] = x
-        sample.target[1] = x
-        sample.target[2] = -y
-        sample.target[3] = y
-        test_vehicle_writer.write sample
-    end
+	# controls
+	widget.horizontalSlider_1.connect(SIGNAL('valueChanged(int)'))do |x|
+		sample.target[0] = x/100.0
+		sample.target[2] = x/100.0
+		test_vehicle_writer.write sample
+	end
+	widget.horizontalSlider_2.connect(SIGNAL('valueChanged(int)'))do |x|
+		sample.target[0] = x/100.0
+		sample.target[2] = -x/100.0
+		test_vehicle_writer.write sample
+	end
+	widget.horizontalSlider_3.connect(SIGNAL('valueChanged(int)'))do |x|
+		sample.target[1] = x/100.0
+		sample.target[3] = x/100.0
+		test_vehicle_writer.write sample
+	end
+	widget.horizontalSlider_4.connect(SIGNAL('valueChanged(int)'))do |x|
+		sample.target[1] = x/100.0
+		sample.target[3] = -x/100.0
+		test_vehicle_writer.write sample
+	end
+	widget.horizontalSlider_5.connect(SIGNAL('valueChanged(int)'))do |x|
+		sample.target[4] = x/100.0
+		sample.target[5] = x/100.0
+		test_vehicle_writer.write sample
+	end
+	widget.horizontalSlider_6.connect(SIGNAL('valueChanged(int)'))do |x|
+		sample.target[4] = x/100.0
+		sample.target[5] = -x/100.0
+		test_vehicle_writer.write sample
+	end
 
-    Vizkit.display simulation
-    Vizkit.display test_vehicle_actuactors
+#    Vizkit.display simulation
+#    Vizkit.display test_vehicle_actuactors
     widget.show 
     Vizkit.exec
 
--- tasks/TestVehicleActuators.cpp
+++ tasks/TestVehicleActuators.cpp
@@ -74,7 +74,7 @@ void TestVehicleActuators::updateHook()
         pwm.push_back(command.target[3]);
         pwm.push_back(command.target[4]);
         pwm.push_back(command.target[5]);
-        Simulation::getAvalonPlugin()->setTestVehicleTarget(pwm);
+	Simulation::getAvalonPlugin()->setTestVehicleTarget(pwm);
 
 
         // write actuator status
@@ -91,28 +91,6 @@ void TestVehicleActuators::updateHook()
         // TODO: Motorstate: current,position,positionExtern
         _status.write(status);
     }
-
-    base::samples::RigidBodyState pose;
-    Eigen::Quaterniond q;
-    Eigen::Vector3d p;
-    Simulation::getAvalonPlugin()->getTestVehiclePose(p,q);
-
-    Eigen::Vector3d lin_vel;
-    Eigen::Vector3d ang_vel;
-
-    Simulation::getAvalonPlugin()->getTestVehicleVelocities(lin_vel, ang_vel);
-
-    lin_vel = q.inverse() * lin_vel;
-    ang_vel = q.inverse() * ang_vel;
-
-    pose.position = p;
-    pose.time = base::Time::now();
-    pose.orientation = q;
-    pose.velocity = lin_vel;
-    pose.angular_velocity = ang_vel;
-
-    _pose_samples.write(pose);
-
 }
 void TestVehicleActuators::errorHook()
 {
